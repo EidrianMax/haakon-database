41d14b9c7c5257db4a6cd988f24272f11ff84da5
Adrian <eidrian.uct@gmail.com>
2022-04-12 17:53:45 +0200

    scrape data from rawg

---
+++ populate/scrape-data.js
@@ -0,0 +1,95 @@
+/* eslint-disable indent */
+/* eslint-disable camelcase */
+
+require('dotenv').config()
+const axios = require('axios')
+const mongoose = require('mongoose')
+const { models: { Game, Platform, Genre } } = require('../index')
+
+const { MONGO_DB_URI, API_KEY } = process.env
+
+    ; (async function () {
+        try {
+            await mongoose.connect(MONGO_DB_URI)
+
+            await Platform.deleteMany()
+            await Game.deleteMany()
+            await Genre.deleteMany()
+
+            const res = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}`)
+
+            const { results } = res.data
+
+            const insertions = results.map(async ({ id, name, released, background_image, metacritic, platforms, genres, short_screenshots }) => {
+                const game = {}
+
+                const screenshots = short_screenshots.map(({ image }) => {
+                    const a = image
+
+                    return a
+                })
+
+                await Promise.all(
+                    platforms.map(async item => {
+                        const platform = {
+                            name: item.platform.name
+                        }
+
+                        try {
+                            await Platform.create(platform)
+                        } catch (error) {
+                            if (error.code === 11000) { console.log(error.code) }
+                        }
+                    })
+                )
+
+                const _platforms = await Promise.all(
+                    platforms.map(async item => {
+                        const findPlatform = await Platform.findOne({ name: item.platform.name })
+                        return findPlatform._id
+                    })
+                )
+
+                await Promise.all(
+                    genres.map(async genre => {
+                        const _genre = {
+                            name: genre.name
+                        }
+
+                        try {
+                            await Genre.create(_genre)
+                        } catch (error) {
+                            if (error.code === 11000) { console.log(error.code) }
+                        }
+                    })
+                )
+
+                const _genres = await Promise.all(
+                    genres.map(async genre => {
+                        const findGenre = await Genre.findOne({ name: genre.name })
+                        return findGenre._id
+                    })
+                )
+
+                const { data: { website, description_raw } } = await axios.get(`https://api.rawg.io/api/games/${id}?key=8cc91cc3d7094411940ec44617d66d39`)
+
+                game.name = name
+                game.released = released
+                game.backgroundImage = background_image
+                game.score = metacritic
+                game.screenshots = screenshots
+                game.platforms = _platforms
+                game.genres = _genres
+                game.description = description_raw
+                game.website = website
+
+                await Game.create(game)
+            })
+
+            await Promise.all(insertions)
+
+            await mongoose.disconnect()
+        } catch (error) {
+            console.error(error)
+        }
+    })()
